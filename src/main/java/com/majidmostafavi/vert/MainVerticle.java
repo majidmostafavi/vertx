d59c2b7d522a33734b468704a2c5f502413f8e50
Majid <majid@majidmostafavi.com>
2023-10-04 01:47:17 +0330

    First Commit

---
+++ src/main/java/com/majidmostafavi/vert/MainVerticle.java
@@ -0,0 +1,29 @@
+package com.majidmostafavi.vert;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+  public static  void main(String[] args){
+    var vertex = Vertx.vertx();
+    vertex.deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello Majid Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
